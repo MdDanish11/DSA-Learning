b446f2ef3accacfa171d481a4b0d61257e1dcc2d
Md Danish <165379365+MdDanish11@users.noreply.github.com>
2025-12-26 13:57:56 +0000

    user input output

---
+++ 01_Basics/InputOutput.java
@@ -0,0 +1,8 @@
+// https://takeuforward.org/plus/dsa/problems/input-output
+
+class Solution {
+    public void printNumber(Scanner sc) {
+        int n = sc.nextInt();
+        System.out.print(n);
+    }
+}
