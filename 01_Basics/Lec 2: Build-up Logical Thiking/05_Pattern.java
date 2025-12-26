46b972e213f6ee3f29097db58d0f1f620fd53b76
Md Danish <165379365+MdDanish11@users.noreply.github.com>
2025-12-26 15:12:33 +0000

    solve pattern 5

---
+++ 01_Basics/Lec 2: Build-up Logical Thiking/05_Pattern.java
@@ -0,0 +1,20 @@
+// https://www.naukri.com/code360/problems/seeding_6581892?utm_source=youtube&utm_medium=affiliate&utm_campaign=striver_patternproblems
+
+public class Solution {
+    public static void seeding(int n) {
+        // Write your code here
+        // for(int i=n; i>=1; i--) {
+        //     for(int j=i; j>=1; j--) {
+        //         System.out.print("* ");
+        //     }
+        //     System.out.println();
+        // }
+
+        for(int i=1; i<=n; i++) {
+            for(int j=1; j<= n-i+1; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+} 
