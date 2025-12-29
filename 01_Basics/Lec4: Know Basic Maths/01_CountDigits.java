f62801c56f8d5d52e4b51b37f86f2e88e25582f6
Md Danish <165379365+MdDanish11@users.noreply.github.com>
2025-12-29 18:18:30 +0000

    another ques on countDigit

--- 01_Basics/Lec4: Know Basic Maths/01_CountDigits.java
+++ 01_Basics/Lec4: Know Basic Maths/01_CountDigits.java
@@ -13,3 +13,23 @@ class Solution {
 
     }
 }
+
+
+// https://www.geeksforgeeks.org/problems/count-digits5716/1
+
+// User function Template for Java
+
+class Solution {
+    static int evenlyDivides(int n) {
+        // code here
+        int count = 0;
+        int originalNum = n;
+        
+        while(n>0) {
+            int digit = n%10;
+            if(digit!=0 && originalNum%digit==0) count++;
+            n/=10;
+        }
+        return count;
+    }
+}
